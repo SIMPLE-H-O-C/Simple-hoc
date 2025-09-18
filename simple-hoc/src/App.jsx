5b9b0e335f5b1a91d98077669df14cabd7342aec
Mpho Itumeleng <mphoitumeleng021@gmail.com>
2025-09-18 12:08:41 +0200

    Updated app.jsx
    added section on homepage

--- simple-hoc/src/App.jsx
+++ simple-hoc/src/App.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import HomePage from "./pages/HomePage";
 import "./App.css";
+import ClientTestimonials from "./components/ClientTestimonials";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <ClientTestimonials />
     </div>
   );
 }
