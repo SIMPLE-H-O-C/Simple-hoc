5a7e0aa5e87ea47642e705ce9c834f5bf8c2280e
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2025-09-19 17:39:18 +0200

    updated page with links to navbar, home and services

--- simple-hoc/src/pages/HomePage.jsx
+++ simple-hoc/src/pages/HomePage.jsx
@@ -1,29 +1,20 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
 
 const HomePage = () => {
   return (
     <div>
-     
-      {/* Placeholder sections for smooth scrolling */}
-      <section id="aboutus" style={{ height: "100vh", padding: "4rem" }}>
-        <h2>Navbar</h2>
-      </section>
-
-      
-      {/* Placeholder sections for smooth scrolling */}
-      <section id="aboutus" style={{ height: "100vh", padding: "4rem" }}>
-        <h2>Home</h2>
-      </section>
+      <Navbar />
+      <Hero />
 
       {/* Placeholder sections for smooth scrolling */}
       <section id="aboutus" style={{ height: "100vh", padding: "4rem" }}>
         <h2>About Us</h2>
       </section>
 
-      <section id="services" style={{ height: "100vh", padding: "4rem", background: "#f8f9fa" }}>
-        <h2>Services</h2>
-      </section>
+      <Services />
 
       <section id="work" style={{ height: "100vh", padding: "4rem" }}>
         <h2>Work</h2>
