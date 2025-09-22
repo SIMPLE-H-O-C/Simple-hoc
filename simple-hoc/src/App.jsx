90c54b06244181ea793cbcad21dba72e9e65630e
Sharleen10 <sharleensshabangu@gmail.com>
2025-09-22 12:42:57 +0200

    add  contact us navigation

--- simple-hoc/src/App.jsx
+++ simple-hoc/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
+import ContactUs from "./pages/ContactUs";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+       <ContactUs />
     </div>
   );
+
+  
 }
 
 export default App;
