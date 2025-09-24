7e78bafe88bd9f6f16f8a99de6e5f72b4559d717
Sharleen10 <sharleensshabangu@gmail.com>
2025-09-24 22:09:34 +0200

    Add routes to link each page

--- simple-hoc/src/App.jsx
+++ simple-hoc/src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import "./App.css";
+import AboutUs from "./pages/AboutUs";
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      {/* Temporary Navigation (just to test switching pages) */}
+      <nav style={{ padding: "1rem", background: "#f8f9fa" }}>
+        <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
+        <Link to="/about">About Us</Link>
+    
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutUs />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
