3f53bb11f815b60ce3399b1969d4a4e7edcff288
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2025-09-29 08:52:57 +0200

    Update Hero.jsx

--- simple-hoc/src/components/Hero.jsx
+++ simple-hoc/src/components/Hero.jsx
@@ -1,25 +1,37 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "../styles/Hero.css";
 
 const Hero = () => {
   return (
     <section id="home" className="hero-section">
-      <div className="hero-content">
-        <h1>
-          We Build <span className="highlight">Digital Experience</span> <br />
-          That Matter
-        </h1>
-       
+     <div className="hero-content">
+        <motion.img
+  src="/logo.png"
+  alt="Logo"
+  animate={{
+    y: [0, -10, 0],
+    scale: [1, 1.05, 1],
+  }}
+  transition={{
+    duration: 3,
+    repeat: Infinity,
+    ease: "easeInOut"
+  }}
+/>
+
+
+
         <div className="hero-buttons">
-         
-         <a href="#contact" className="btn btn-outline">
-            Explore Our Work
-          </a> 
+          <a href="#portfolio" className="btn btn-outline">
+            View Work
+          </a>
           <a href="#contact" className="btn btn-outline">
             Get In Touch
           </a>
         </div>
       </div>
+
       <div className="scroll-indicator">
         <span></span>
       </div>
