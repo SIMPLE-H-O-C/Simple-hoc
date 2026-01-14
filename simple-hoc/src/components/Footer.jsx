039ee54d8e382ef37ccf00c38c5d74016b4ba91e
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-01-14 14:23:44 +0200

    Update Footer.jsx

--- simple-hoc/src/components/Footer.jsx
+++ simple-hoc/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-light text-dark text-center py-3 border-top shadow-sm">
-      <p className="mb-0">SIMPLE. H-O-C © 2025</p>
+      <p className="mb-0">SIMPLE-HOC © 2025</p>
     </footer>
   );
 };
